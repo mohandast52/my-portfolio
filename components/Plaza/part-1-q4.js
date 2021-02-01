4a4072050235854b5d5ba4c09929918604f8d393
Mohan Das <mohan.das@gocomet.in>
2021-02-02 02:42:29 +0530

    algorithm added

--- components/Plaza/part-1-q4.js
+++ components/Plaza/part-1-q4.js
@@ -1,16 +1,26 @@
+/* eslint-disable no-console */
 const list = [0, 2, 4, 6, 8];
 const k = 4;
 
-const fn = (total, i, array, finalOutput) => {
+const fn = (total, i, array, finalOp) => {
   if (total === k) {
-    return finalOutput;
+    console.log(finalOp);
+    return true;
   }
 
   if (i === array.length) {
-    return 'None';
+    return false;
   }
+
+  fn(total + array[i], i + 1, array, `${finalOp}+${array[i]}`);
+  fn(total - array[i], i + 1, array, `${finalOp}-${array[i]}`);
 
   return null;
 };
 
 fn(0, 0, list, '');
+
+/**
+ * Time complexity: 2^n as we are either selecting the value or not-selecting
+ * which call recursively.
+ */
